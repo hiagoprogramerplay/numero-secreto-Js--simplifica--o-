7d3ee74cf37c7c4bf628b3352fe164c680f8f259
Hiagoprogramer_play <161398837+hiagoprogramerplay@users.noreply.github.com>
2024-03-13 20:13:13 -0300

    atualizaçao de codigo simplificado

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-let título = document.querySelector ('h1');
+//let título = document.querySelector ('h1');
 título.innerHTML = 'Jogo do número secreto';
 
-let paragráfo = document.querySelector('p');
+//let paragráfo = document.querySelector('p');
 paragráfo.innerHTML = 'Escolha um número entre 1 e 10'; 
 
-function verificarChute() {
+//function verificarChute() {
+    console.log('O botão foi clicado!')
+//};
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;    
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p','Escolha um número entre 1 e 100' );
+
+functio verificarChute() {
     console.log('O botão foi clicado!')
 };
