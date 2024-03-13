f126b5d2b17ca242e331fa36edc6577028f8cb49
Hiagoprogramer_play <161398837+hiagoprogramerplay@users.noreply.github.com>
2024-03-13 20:04:39 -0300

    começãndo projeto em java

---
+++ app.js
@@ -0,0 +1,9 @@
+let título = document.querySelector ('h1');
+título.innerHTML = 'Jogo do número secreto';
+
+let paragráfo = document.querySelector('p');
+paragráfo.innerHTML = 'Escolha um número entre 1 e 10'; 
+
+function verificarChute() {
+    console.log('O botão foi clicado!')
+};
