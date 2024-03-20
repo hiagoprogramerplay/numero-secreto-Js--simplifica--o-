759a5a0a2354b80a895c2ac293896500d3082103
Hiagoprogramer_play <161398837+hiagoprogramerplay@users.noreply.github.com>
2024-03-20 19:32:19 -0300

    ATUALIZAÇÃO DE VISUALIZAÇÃO
    retirando a visualização do console.log para adicionar os textos de acerto na tela diretamente ao usuário.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let numeroSecreto = gerarNumeroAleatorio();
+let numeroSecreto = 7;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -10,7 +10,11 @@ exibirTextoNaTela('p','Escolha um número entre 1 e 10' );
 
 function verificarChute() {
     let chute =  document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'acertou!')
+        
+    }
 }
 
 function gerarNumeroAleatorio() {
