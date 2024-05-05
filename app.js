fb6e7d87ea876df7a61562ebce0d255303600027
Hiagoprogramer_play <161398837+hiagoprogramerplay@users.noreply.github.com>
2024-05-05 12:31:27 -0300

    voltando etapas antes dos textes

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'Jogo DO Adivinha');
-    exibirTextoNaTela('p','ACERTE A DATA DO NOSSO CASAMENTO');
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p','Escolha um número entre 1 e 10');
 }
 
 exibirMensagemInicial();
