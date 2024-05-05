e86bb1ef4cfbf6f29b4c2caf7b3046b64145ef91
Hiagoprogramer_play <161398837+hiagoprogramerplay@users.noreply.github.com>
2024-05-05 12:24:01 -0300

    atualização teste

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let listaDeNumerosSorteados = [];
 let numeroLimite = 10;
-let numeroSecreto = gerarNumeroAleatorio();
+let numeroSecreto = 13; //gerarNumeroAleatorio();
 let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
@@ -10,8 +10,8 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p','Escolha um número entre 1 e 10');
+    exibirTextoNaTela('h1', 'Jogo DO Adivinha');
+    exibirTextoNaTela('p','ACERTE A DATA DO NOSSO CASAMENTO');
 }
 
 exibirMensagemInicial();
